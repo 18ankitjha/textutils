80c66e75cecf75415fa7eae8f8e8d24ba5bf2947
ANKIT JHA <ankitjha01aj@gmail.com>
2023-01-14 17:00:39 +0530

    Updates

---
+++ precache-manifest.5202eb3c9d5744c3c3a0828478671d44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f24330b376923f78f794",
+    "url": "/textutils/static/css/main.c1eea837.chunk.css"
+  },
+  {
+    "revision": "f24330b376923f78f794",
+    "url": "/textutils/static/js/main.f24330b3.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/textutils/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "b39825fafff6e43d1271",
+    "url": "/textutils/static/js/2.b39825fa.chunk.js"
+  },
+  {
+    "revision": "249b581e5cf121412222",
+    "url": "/textutils/static/js/runtime~main.249b581e.js"
+  },
+  {
+    "revision": "7fbebbdd9f5c87515bc926d108a1c893",
+    "url": "/textutils/index.html"
+  }
+];
